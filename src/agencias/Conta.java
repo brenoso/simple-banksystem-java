a1cc45d0ea8ad4fd8ba723f1513def3fef8bac43
Breno Oliveira <brn753@gmail.com>
2015-08-27 18:04:59 -0300

    commit
    added and fixed some methods

--- src/agencias/Conta.java
+++ src/agencias/Conta.java
@@ -1,14 +1,23 @@
 package agencias;
 
+import java.util.Scanner; // importação da biblioteca de leitura de dados do teclado
+
+
 public class Conta {
+    
+Scanner teclado = new Scanner(System.in); // declaração de objeto para entrada de dados.
+
 
        int numero;
        double saldo;
        double limite;
-       Cliente titular; // declaração de titular do tipo de classe Cliente.
+       Cliente titular = new Cliente(); // declaração de titular do tipo de classe Cliente.
        
-       void saca (double quantidade) { // método para sacar determinada qntdade.
-                     
+       void saca () { // método para sacar determinada qntdade.
+         
+           System.out.println("\nDigite um valor para saque:");
+           double quantidade= teclado.nextDouble();
+           
            if(quantidade > this.saldo)
            {
                System.out.println("Quantidade indisponível para saque.");
@@ -21,8 +30,13 @@ public class Conta {
            }
        }
 
-       void deposita (double quantidade){ // método para depositar uma qntdade.
+       void deposita (){ // método para depositar uma qntdade.
+       
+       System.out.println("Digite um valor para depósito: ");
+       double quantidade =teclado.nextDouble();
+       
        this.saldo+= quantidade;
+ 
        if (this.saldo > this.limite)
        {
            System.out.println("Impossível depositar. Limite estourado.");
@@ -33,16 +47,46 @@ public class Conta {
            System.out.println("Deposito Realizado com Sucesso!");
            System.out.println("Novo Saldo:"+this.saldo);
        }
-
-      
-       
-      
        }
                
        void transferePara (Conta destino, double quantidade){
            
        this.saldo-= quantidade;
        destino.saldo+=quantidade;
+       }
+       
+       void extrato(){
+       
+        System.out.println("\n\n        Dados     ");
+        System.out.println("Titular : "+this.titular.nome +" " +this.titular.sobrenome);
+        System.out.println("CPF: "+this.titular.cpf);
+        System.out.println("Saldo: "+this.saldo);
+        System.out.println("Limite: "+this.limite);
+        System.out.println("Numero da conta: "+this.numero);
+       
+       }
+       
+       void cadastro (){
+         
+        System.out.println("Digite o nome do proprietário da conta: ");
+        this.titular.nome = teclado.nextLine();
+        
+        System.out.println("Digite o sobrenome do proprietário da conta: ");
+        this.titular.sobrenome = teclado.nextLine();
+        
+        System.out.println("Digite o cpf do proprietário da conta: ");
+        this.titular.cpf = teclado.nextLine();
+              
+        System.out.println("Digite um numero para conta: ");
+        this.numero = teclado.nextInt();
+        
+        System.out.println("Digite um saldo inicial para conta: ");
+        this.saldo = teclado.nextDouble();
+               
+        System.out.println("Digite o limite da conta: ");
+        this.limite= teclado.nextDouble();
+        
+        System.out.format("\nA conta de %s foi criada com sucesso!",this.titular.nome);
        }
 }
 
--- src/agencias/Main.java
+++ src/agencias/Main.java
@@ -1,52 +1,20 @@
 package agencias;
-
 import java.util.Scanner; // importação da biblioteca de leitura de dados do teclado
+
 
 
 public class Main {
     public static void main(String[] args) {
-        
-    Conta conta1; // declaração da variável conta1 do tipo Conta (classe Conta)
-    conta1 = new Conta (); // instanciação do objeto conta1
     
-    Cliente cliente1 = new Cliente(); // criação da variável cliente1 do tipo de classe Cliente
-    conta1.titular = cliente1; // atribuição da variavel cliente1 para o atributo titular do objeto conta1.
-   
+    Scanner teclado = new Scanner(System.in);// declaração de objeto para entrada de dados.
+    
+    Conta conta1 = new Conta();
 
 
-    Scanner teclado = new Scanner(System.in); // declaração de objeto para entrada de dados.
-
-        System.out.println("Digite o nome do proprietário da conta: ");
-        conta1.titular.nome = teclado.nextLine();
-        
-        System.out.println("Digite o sobrenome do proprietário da conta: ");
-        conta1.titular.sobrenome = teclado.nextLine();
-        
-        System.out.println("Digite o cpf do proprietário da conta: ");
-        conta1.titular.cpf = teclado.nextLine();
-              
-        System.out.println("Digite um numero para conta: ");
-        conta1.numero = teclado.nextInt();
-        
-        System.out.println("Digite um saldo inicial para conta: ");
-        conta1.saldo = teclado.nextDouble();
-               
-        System.out.println("Digite o limite da conta: ");
-        conta1.limite= teclado.nextDouble();
-        
-        System.out.format("\nA conta de %s foi criada com sucesso!",conta1.titular.nome);
-        System.out.println("\n\nDados: ");
-        System.out.println("Titular : "+conta1.titular.nome +" " +conta1.titular.sobrenome);
-        System.out.println("CPF: "+conta1.titular.cpf);
-        System.out.println("Saldo: "+conta1.saldo);
-        System.out.println("Limite: "+conta1.limite);
-        System.out.println("Numero da conta: "+conta1.numero);
-        
-        System.out.println("\nDigite um valor para saque:");
-        conta1.saca(teclado.nextDouble()); // método conta1.saca(double) tendo como passagem por referencia o método teclado.nextDouble().
-
-        System.out.println("Digite um valor para depósito: ");
-        conta1.deposita(teclado.nextDouble()); // deposito de 200 reais.
+    conta1.cadastro();
+    conta1.extrato();
+    conta1.saca();
+    conta1.deposita();
 
     }
 
